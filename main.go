f835a2697c0afc83c0b1f570e74e79720c157920
gucchisk <gucchi_sk@yahoo.co.jp>
2023-01-23 02:18:49 +0900

    v1.0.3

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ var version = "unknown"
 func main() {
 	vf := flag.Bool("version", false, "version flag")
 	bif := flag.Bool("info", false, "build info flag")
+	sf := flag.Bool("settings", false, "settings flag")
 	flag.Parse()
 	buildInfo, ok := debug.ReadBuildInfo()
 	if !ok {
@@ -23,9 +24,14 @@ func main() {
 		return
 	} else if *bif {
 		main := buildInfo.Main
+		fmt.Printf("go: %s\n", buildInfo.GoVersion)
 		fmt.Printf("path: %s\n", main.Path)
 		fmt.Printf("sum: %s\n", main.Sum)
 		return
+	} else if *sf {
+		for _, setting := range buildInfo.Settings {
+			fmt.Printf("%s: %s\n", setting.Key, setting.Value)
+		}
 	}
 	fmt.Printf("This is sample command %s\n", version)
 }
